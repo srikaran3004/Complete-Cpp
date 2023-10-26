54fba812332bff812abcee77cc5dd466414a9021
Srikaran Devarakonda <114800064+srikaran3004@users.noreply.github.com>
2023-10-26 09:38:40 +0530

    C++

---
+++ overload+operator.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class sum
+{
+    public:
+    int b,a;
+    sum()
+    {
+        b=50;
+    }
+    void set_data()
+    {
+        cout<<"Enter a : ";
+        cin>>a;
+    }
+    int operator+(sum s1)
+    {
+        return(a+s1.b);
+    }
+};
+int main()
+{
+    int d;
+    sum s,t;
+    s.set_data();
+    d=s+t;
+    cout<<"SUM IS: "<<d;
+}
